781c9e734802cb83fde00a5f2430e3efe0659211
artifexpereo <pvlkhd@ukr.net>
2023-09-20 12:05:19 +0300

    16th homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Post from "./Post";
 
-function App() {
+const ANAKIN_IMAGE =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWOey035wnlxX7w0AzG8s0rQ70cwaT0k1JA&usqp=CAU";
+
+const RAY_IMAGE =
+  "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Post
+      author={{
+        name: "Anakin Skywalker",
+        photo: ANAKIN_IMAGE,
+        nickname: "@dart_vader",
+      }}
+      content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+      image={RAY_IMAGE}
+      date={"26 лют."}
+    />
   );
-}
+};
 
 export default App;
